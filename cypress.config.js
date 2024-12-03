b3b554cb6ac6ec686553ba2f8c859c405f1958f3
Kholofelo  Masela <KMasela@jhbdvt.co.za>
2024-12-04 00:46:53 +0200

    added mochawesome for reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,35 @@
 const { defineConfig } = require("cypress");
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 module.exports = defineConfig({
   defaultCommandTimeout: 8000,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reporterEnabled: 'mochawesome',
+    mochawesomeReporterOptions: {
+      reportDir: 'cypress/reports/mocha',
+      quite: true,
+      overwrite: false,
+      html: false,
+      json: true,
+    },
+  },
   e2e: {
+   
     baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php/auth',
     viewportHeight : 550,
     experimentalRunAllSpecs : true,
     setupNodeEvents(on, config) {
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        await beforeRunHook(details);
+      });
+
+      on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+      });
+    
       // implement node event listeners here
       
     },
